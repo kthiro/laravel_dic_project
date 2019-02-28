c717fabf62964a127386ef7c5e4bc7cb02e62b50
kthiro <1b14kwy@gmail.com>
2019-02-28 09:28:03 +0000

    Laravelインストール * install_guestos.sh LaravelのインストールにはPHP7.1.3以上が必要だったため、ゲストOSでの環境構築手順を修正。vagrant上に構築したubuntu-16.04のデフォルトのパッケージのままPHPをインストールするとバージョンが7.0.Xであるため、外部リポジトリであるppa:ondrej/phpを使用してPHP7.3.Xをインストール。
    * README.md
    ゲストOSの環境構築手順を修正認め、READEME.mdも修正

--- README.md
+++ README.md
@@ -15,6 +15,43 @@
 ### ゲストOS上でインストールしたもの
 * nginx バージョン 1.10.3
 * MySQL バージョン 5.7.25
-* PHP バージョン 7.0.33
+* PHP バージョン 7.3.2
 * composer バージョン 1.8.4
-* Laravel バージョン 2.0.1
+* Laravel バージョン 5.8.2
+
+## 環境構築手順
+### ホストOS側での作業
+1. VirtualBoxのインストール
+    * インストール元：<https://www.virtualbox.org/wiki/Downloads>からのインストールが必要
+    * バージョン 6.0.0 を使用
+2. vagrantのインストール
+    * インストール元：<https://www.vagrantup.com/downloads.html>からのインストールが必要
+    * バージョン 2.2.2 を使用
+3. 任意のディレクトリで次のコマンドを実行する。
+    ```
+    git clone git@github.com:kthiro/laravel_dic_project.git
+    cd laravel_dic_project
+    vagrant up
+    vagrant ssh
+    ```
+----------------------
+### ゲストOS側での作業
+1. install_guestos.shを実行する。
+    ```
+    cd /vagrant && chmod u+x install_guestos.sh && ./install_guestos.sh
+    ```
+2. Laravelのプロジェクトディレクトリ（sportweb/ ディレクトリ）で composer のインストール
+    ```
+    cd sportweb
+    composer install
+    ```
+-----------------------
+### アプリケーションの起動確認
+1. ゲストOS上でLaravelサーバーの起動
+    ```
+    php artisan serve --host=192.168.38.10 --port=8000
+    ```
+2. ホストOS上のブラウザから次のアドレスへアクセス
+    ```
+    192.168.38.10:8000
+    ```
--- install_guestos.sh
+++ install_guestos.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-# ゲストOSのアップデート
-sudo apt-get upgrade
+# パッケージリストの更新
+sudo apt-get update && sudo apt-get -y upgrade
+
+# software-properties-commonパッケージをインストール（add-apt-repositoryコマンドを使用するため）
+sudo apt-get install software-properties-common
+
+# ppa:ondrej/php リポジトリを追加（PHP7.3をインストールするためにこの外部リポジトリが必要）
+sudo add-apt-repository -y ppa:ondrej/php
+
+# パッケージリストを更新
+sudo apt-get update
 
 # nginxのインストール
-sudo apt-get install -y nginx php-fpm
+sudo apt-get -y install nginx
 
 # PHPのインストール
-sudo apt-get install -y php-mysql php-mbstring php-zip unzip
+sudo apt-get -y install php7.3
+sudo apt-get -y install php-pear php7.3-curl php7.3-dev php7.3-gd php7.3-mbstring php7.3-zip php7.3-mysql php7.3-xml unzip
 
 # MySOLのインストール
-sudo apt-get install -y mysql-server mysql-client
+sudo apt-get -y install mysql-server mysql-client
 
 # mysqld.cnfの編集：ホストOSからもMySQLへアクセスできるよう、bind addressを192.168.37.10へ変更
 sudo sed -i 's/127.0.0.1/192.168.38.10/' /etc/mysql/mysql.conf.d/mysqld.cnf
@@ -19,7 +29,7 @@ sudo sed -i 's/127.0.0.1/192.168.38.10/' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo service mysql restart
 
 # composerのインストール
-curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer
+cd /vagrant && curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer
 
 # (参考)composerのアップデートコマンド
 # composer self-update
