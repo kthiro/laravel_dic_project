bbf8cdae9e63e1139ba88b0cdfa39a59c5f0e432
kthiro <1b14kwy@gmail.com>
2019-03-05 14:58:13 +0900

    (WIP)会員登録内容の確認画面を実装〜バリデーションを実装してみる
    * resources/views/collections/generate_form_using_table.blade.php
    バリデーションエラー発生時のエラーメッセージの表示を実装
    
    * resources/views/layouts/application.blade.php
    ヘッダーのナビゲーションメニューのリンクの修正

--- sportweb/resources/views/collections/generate_form_using_table.blade.php
+++ sportweb/resources/views/collections/generate_form_using_table.blade.php
@@ -13,6 +13,13 @@
             class="form-control"
             placeholder="{{$form_element['input_attribute_placeholder']}}"
         >
+        
+        @if ($errors->has($form_element['input_attribute_name']))
+            @foreach ($errors->get($form_element['input_attribute_name']) as $error_msg)
+                <small class="text-danger">{{$error_msg}}</small><br>
+            @endforeach
+        @endif
+
         <small class="text-info" id="{{$form_element['info_attribute_id']}}">
             {{$form_element['info_content']}}
         </small>
--- sportweb/resources/views/layouts/application.blade.php
+++ sportweb/resources/views/layouts/application.blade.php
@@ -20,7 +20,7 @@
     </head>
     <body>
         <nav class='navbar navbar-expand-md navbar-light fixed-top bg-light'>
-            <a class='navbar-brand' href='/'>Sportweb</a>
+            <a class='navbar-brand' href='/top'>Sportweb</a>
             <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'>
                 <span class='navbar-toggler-icon'></span>
             </button>
